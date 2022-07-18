80a4d9dba3b94715f7d333c3b4204d5d5fef224c
Federico Gonzalez <alambratore@hotmail.com>
2022-07-18 19:00:24 -0300

    Modificación en insecureAuth

--- database/conection.js
+++ database/conection.js
@@ -5,9 +5,7 @@ const dataBase = mysql.createConnection({
   host: process.env.MARIADB_HOST,
   user: process.env.MARIADB_USER,
   password: process.env.MARIADB_PASSWORD,
-  database: process.env.MARIADB_DATABASE,
-  connectTimeout: 10000,
-  insecureAuth: true
+  database: process.env.MARIADB_DATABASE
 });
 
 module.exports = dataBase;
