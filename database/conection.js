26f8c352b8e28abef9d46943a598da26e5d62214
Federico Gonzalez <alambratore@hotmail.com>
2022-07-18 17:17:41 -0300

    debug MySQL connection

--- database/conection.js
+++ database/conection.js
@@ -1,11 +1,18 @@
 // CONEXIÓN A LA BASE DE DATOS (modificar los datos del archivo .env)
-const mysql = require('mysql')
+const mysql = require('mysql');
 
 const dataBase = mysql.createConnection({
   host: process.env.MARIADB_HOST,
   user: process.env.MARIADB_USER,
   password: process.env.MARIADB_PASSWORD,
   database: process.env.MARIADB_DATABASE
-})
+});
 
-module.exports = dataBase
+console.log({
+  host: process.env.MARIADB_HOST,
+  user: process.env.MARIADB_USER,
+  password: process.env.MARIADB_PASSWORD,
+  database: process.env.MARIADB_DATABASE
+});
+
+module.exports = dataBase;
